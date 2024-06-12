656d338ce0f9a743aa70a7e06b89b2cc01a234fc
Miguel-Silva-Alves <miguelsilvalves.2015@gmail.com>
2024-06-12 16:49:31 -0300

    :sparkles: feat: add preferences of remetente

--- app/src/main/java/com/example/pdmchat/ui/SendMessageActivity.kt
+++ app/src/main/java/com/example/pdmchat/ui/SendMessageActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -27,6 +29,11 @@ class SendMessageActivity : AppCompatActivity() {
     private val messageController: MessageCreateRtController by lazy {
         MessageCreateRtController(this)
     }
+
+    // AutoComplete users
+    private lateinit var recipientsAdapter: ArrayAdapter<String>
+    private lateinit var recipientsList: MutableList<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Binding
@@ -39,7 +46,6 @@ class SendMessageActivity : AppCompatActivity() {
             sendMessageBinding.chatSendler.setText(name)
         }
 
-
         sendMessageBinding.sendButton.setOnClickListener{
             val sendler = sendMessageBinding.chatSendler.text.toString()
             val recipient = sendMessageBinding.recipientEditText.text.toString()
@@ -47,14 +53,43 @@ class SendMessageActivity : AppCompatActivity() {
             sendMessage(sendler, recipient, messageText)
         }
 
+        // Auto complete recipients
+        recipientsList = mutableListOf()
+
+        val sharedPreferencesRecipients = getSharedPreferences("Recipients", Context.MODE_PRIVATE)
+        val destinatariosSet = sharedPreferencesRecipients.getStringSet("Recipients", emptySet())
+        Log.e("destinatariosSet", destinatariosSet.toString())
+        recipientsList.addAll(destinatariosSet ?: emptySet())
+
+        recipientsAdapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_dropdown_item_1line,
+            recipientsList
+        )
+        sendMessageBinding.recipientEditText.setAdapter(recipientsAdapter)
+
+
     }
 
     private fun sendMessage(sendler: String?, recipient: String, conteudo: String) {
 
+        // Change ou Create sendler
         val sharedPreferences = getSharedPreferences("Sendler", Context.MODE_PRIVATE)
         val editor = sharedPreferences.edit()
         editor.putString("Sendler", sendler)
         editor.apply()
+
+        // Update recipients
+        if (!recipientsList.contains(recipient)) {
+            Log.e("recipientsList", recipient)
+            recipientsList.add(recipient)
+            recipientsAdapter.notifyDataSetChanged()
+
+            val sharedPreferencesRecipients = getSharedPreferences("Recipients", Context.MODE_PRIVATE)
+            val editorRec = sharedPreferencesRecipients.edit()
+            editorRec.putStringSet("Recipients", recipientsList.toSet())
+            editorRec.apply()
+        }
 
         val mensagem = if (conteudo.length > 150) conteudo.substring(0, 150) else conteudo
         val cal = Calendar.getInstance()
